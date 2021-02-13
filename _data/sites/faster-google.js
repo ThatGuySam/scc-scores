b49d6578505ee374f12d859920281dd53bce144b
Sam Carlton <sam@sam.lc>
2021-02-13 15:08:52 -0600

    Add faster-google scores

---
+++ _data/sites/faster-google.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "Google", // optional, falls back to object key
+	description: "Comparing Google to Faster Google",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		'https://fastergoogle.com/',
+		'https://www.google.com/'
+	]
+};
