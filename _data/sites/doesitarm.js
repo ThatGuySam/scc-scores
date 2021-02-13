668964f4781a5b0b1d156b24ea057597f788bb32
Sam Carlton <sam@sam.lc>
2021-02-13 15:30:10 -0600

    Add tests for Does It ARM

---
+++ _data/sites/doesitarm.js
@@ -0,0 +1,46 @@
+module.exports = {
+	name: "Does It ARM?", // optional, falls back to object key
+	description: "Pages for Does it ARM",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		'https://doesitarm.com/',
+
+		// Formulae
+		'https://doesitarm.com/kind/homebrew/',
+		'https://doesitarm.com/formula/neovim/',
+		'https://doesitarm.com/formula/postgresql',
+		'https://doesitarm.com/formula/aom/',
+		
+
+		// Apps
+		'https://doesitarm.com/app/spotify/',
+		'https://doesitarm.com/app/spotify/benchmarks/',
+		'https://doesitarm.com/app/discord/',
+		'https://doesitarm.com/app/discord/benchmarks/',
+		'https://doesitarm.com/app/nodejs/',
+
+		// Games
+		'https://doesitarm.com/games/',
+		'https://doesitarm.com/game/a-total-war-saga-troy/',
+		'https://doesitarm.com/game/a-total-war-saga-troy/benchmarks/',
+		'https://doesitarm.com/game/minecraft/',
+		'https://doesitarm.com/game/genshin-impact/',
+		'https://doesitarm.com/game/league-of-legends/',
+
+
+		// TV
+		'https://doesitarm.com/benchmarks/',
+		'https://doesitarm.com/tv/how-to-install-node-js-on-mac-apple-m1-chip-i-re2e3vcrptc/',
+		'https://doesitarm.com/tv/visual-studio-code-for-apple-m1-is-here-i-2dreoc4wx7m/',
+		'https://doesitarm.com/tv/android-studio-on-apple-silicon-m1-i-qs4ydzc8yoq/',
+
+
+		// Categories
+		'https://doesitarm.com/categories/',
+		'https://doesitarm.com/kind/science-and-research-software/',
+		'https://doesitarm.com/kind/developer-tools/',
+		'https://doesitarm.com/kind/vpns-security-and-privacy',
+	]
+};
